c02609c0d66094fbe4c2c07e416cb63d1418b447
ngocthien0507 <48592936+ngocthien0507@users.noreply.github.com>
2019-07-23 21:40:32 +0700

    Add_TabBar

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity,ScrollView, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import Icon from "react-native-vector-icons/MaterialIcons";
 import Icon1 from "react-native-vector-icons/Ionicons";
 import Icon2 from "react-native-vector-icons/Feather";
-
+import { createAppContainer, createBottomTabNavigator, createStackNavigator, Header } from 'react-navigation';
 const POLO_BLUE_COLOR = 'rgb(51,60,87)';
 const FOLLOW_COLOR = 'rgb(71,113,246)';
 const SEND_MESSAGE_COLOR = 'rgb(120,213,250)';
@@ -25,122 +25,159 @@ const onPressFollowButton = () => {
 const onPressSendMessButton = () => {
   Alert.alert('Message sended!');
 }
-export default class App extends React.Component{
-  render(){
+class HomeScreen extends React.Component {
+  render() {
     return (
       <View style={styles.container}>
-          <View style={styles.HeaderBar}>
+        <View style={styles.HeaderBar}>
           <TouchableOpacity style={styles.HeaderBarIcon}>
-                    <Icon1
-                      name="ios-arrow-back"
-                      color="#a8a8a8"
-                      size={35}
-                    />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.HeaderBarIcon}>
-                    <Icon
-                      name="photo-filter"
-                      color="#a8a8a8"
-                      size={35}
-                    />
-          </TouchableOpacity>
-          </View>
-          <View style={styles.HeaderGroup}>
-            <View style={styles.AvatarCol}>
-            <Image style={styles.Avatar}
-            source={require('./assets/avatar1.jpg')}
+            <Icon1
+              name="ios-arrow-back"
+              color="#a8a8a8"
+              size={35}
             />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.HeaderBarIcon}>
+            <Icon
+              name="photo-filter"
+              color="#a8a8a8"
+              size={35}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.HeaderGroup}>
+          <View style={styles.AvatarCol}>
+            <Image style={styles.Avatar}
+              source={require('./assets/avatar1.jpg')}
+            />
+          </View>
+          <View style={styles.InfoCol}>
+            <View style={styles.TextGroup}>
+              <Text style={styles.NameText}>
+                Ngọc Thiện
+                  </Text>
+              <Text style={styles.JobText}>
+                Developer
+                  </Text>
             </View>
-            <View style={styles.InfoCol}>
-                <View style={styles.TextGroup}>
-                  <Text style={styles.NameText}>
-                    Ngọc Thiện
-                  </Text>
-                  <Text style={styles.JobText}>
-                    Developer
-                  </Text>
-                </View>
-                <View style={styles.ButtonGroup}>
-                  <TouchableOpacity onPress={onPressFollowButton} style={styles.FollowButton}>
-                      <Text style={styles.FollowText}>Follow</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={onPressSendMessButton} style={styles.SendMessageButton}>
-                    <Icon
-                      name="send"
-                      color="white"
-                      size={25}
-                    />
-                  </TouchableOpacity>
-                </View>
+            <View style={styles.ButtonGroup}>
+              <TouchableOpacity onPress={onPressFollowButton} style={styles.FollowButton}>
+                <Text style={styles.FollowText}>Follow</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={onPressSendMessButton} style={styles.SendMessageButton}>
+                <Icon
+                  name="send"
+                  color="white"
+                  size={25}
+                />
+              </TouchableOpacity>
             </View>
           </View>
-          <View style={styles.CountGroup}>
-            <View style={styles.CountPhoto}>
-                <Text style={styles.Amount}>
-                  210
+        </View>
+        <View style={styles.CountGroup}>
+          <View style={styles.CountPhoto}>
+            <Text style={styles.Amount}>
+              210
                 </Text>
-                <Text style={styles.CountText}>
-                  Photos
+            <Text style={styles.CountText}>
+              Photos
                 </Text>
+          </View>
+          <View style={styles.CountFollower}>
+            <Text style={styles.Amount}>
+              15k
+                </Text>
+            <Text style={styles.CountText}>
+              Followers
+                </Text>
+          </View>
+          <View style={styles.CountFollowing}>
+            <Text style={styles.Amount}>
+              605
+                </Text>
+            <Text style={styles.CountText}>
+              Following
+                </Text>
+          </View>
+        </View>
+        <ScrollView style={styles.ScrollImages}>
+          <View style={styles.ImageGroup}>
+            <View style={styles.ImgLeftCol}>
+              {imgData.slice(0, centerImgData).map(item => {
+                return <TouchableOpacity>
+                  <Image source={item.imgSource} style={styles.image}></Image>
+                </TouchableOpacity>
+              })}
             </View>
-            <View style={styles.CountFollower}>
-                <Text style={styles.Amount}>
-                  15k
-                </Text>
-                <Text style={styles.CountText}>
-                  Followers
-                </Text>
-            </View>
-            <View style={styles.CountFollowing}>
-                <Text style={styles.Amount}>
-                  605
-                </Text>
-                <Text style={styles.CountText}>
-                  Following
-                </Text>
+            <View style={styles.ImgRightCol}>
+              {imgData.slice(centerImgData).map(item => {
+                return <TouchableOpacity>
+                  <Image source={item.imgSource} style={styles.image}></Image>
+                </TouchableOpacity>
+              })}
             </View>
           </View>
-          <ScrollView style={styles.ScrollImages}>
-            <View style={styles.ImageGroup}>
-              <View style={styles.ImgLeftCol}>
-                {imgData.slice(0,centerImgData).map(item=>{
-                  return <Image source={item.imgSource} style={styles.image}></Image>
-                })}
-              </View>
-              <View style={styles.ImgRightCol}>
-                {imgData.slice(centerImgData).map(item=>{
-                  return <Image source={item.imgSource} style={styles.image}></Image>
-                })}
-              </View>
-            </View>  
-          </ScrollView>  
-          <View style={styles.FooterBar}>
-            <TouchableOpacity style={styles.HeaderBarButton}>
-                      <Icon2
-                        name="home"
-                        color="#a8a8a8"
-                        size={35}
-                      />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.HeaderBarButton}>
-                      <Icon2
-                        name="plus-circle"
-                        color="#a8a8a8"
-                        size={35}
-                      />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.HeaderBarButton}>
-                      <Icon2
-                        name="user"
-                        color="#a8a8a8"
-                        size={35}
-                      />
-            </TouchableOpacity>
-          </View>            
+        </ScrollView>
       </View>
     );
   }
 }
+class Screen2 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{fontSize:100}}>User Screen!</Text>
+      </View>
+    );
+  }
+}
+class Screen3 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{fontSize:100}}>Add Images Screen!</Text>
+      </View>
+    );
+  }
+}
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  if (routeName === "Home") {
+    iconName = 'home';
+  }
+  else if (routeName === "Screen2") {
+    iconName = 'user';
+  }
+  else if (routeName === "Screen3") {
+    iconName = 'plus-circle';
+  }
+    return <Icon2 name={iconName} size={35} color={tintColor} />;
+};
+
+const AppTabs = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Screen3: Screen3,
+    Screen2: Screen2,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+          return getTabBarIcon(navigation, focused, tintColor);
+      },
+      tabBarLabel: () => { },
+      tabBarOptions: {
+        activeTintColor: FOLLOW_COLOR,
+        style: {
+          borderTopColor: 'transparent',
+        },
+      },
+    })
+  },
+);
+
+const App = createAppContainer(AppTabs);
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -149,62 +186,62 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     fontFamily: 'sans-serif-thin',
   },
-  HeaderBar:{
-    flex:0.17,
+  HeaderBar: {
+    flex: 0.17,
     flexDirection: "row",
     justifyContent: 'space-between',
-    alignItems:'flex-end',
-    marginLeft:20,
-    marginRight:20,
+    alignItems: 'flex-end',
+    marginLeft: 20,
+    marginRight: 20,
   },
-  HeaderBarButton:{
-    height:40,
-    width:60,
-    alignItems:'center'
+  HeaderBarButton: {
+    height: 40,
+    width: 60,
+    alignItems: 'center'
   },
   HeaderGroup: {
-    flex:0.25,
-    flexDirection:'row',
-    justifyContent:'center'
+    flex: 0.25,
+    flexDirection: 'row',
+    justifyContent: 'center'
   },
-  AvatarCol:{
-    flex:0.4,
+  AvatarCol: {
+    flex: 0.4,
     justifyContent: "center",
-    alignItems:"center"
+    alignItems: "center"
   },
-  Avatar:{
-    height:130,
-    width:130,
-    borderRadius:65
+  Avatar: {
+    height: 130,
+    width: 130,
+    borderRadius: 65
   },
-  InfoCol:{
-    flex:0.5,
-    flexDirection:'column',
-    justifyContent:'space-between',
-    marginLeft:10
+  InfoCol: {
+    flex: 0.5,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginLeft: 10
   },
-  TextGroup:{
+  TextGroup: {
   },
-  NameText:{
-    fontSize:23,
-    fontWeight:"700",
-    color:'black',
+  NameText: {
+    fontSize: 23,
+    fontWeight: "700",
+    color: 'black',
   },
-  JobText:{
-    fontSize:15,
-    fontWeight:"400",
-    color:'#bdbdbd'
+  JobText: {
+    fontSize: 15,
+    fontWeight: "400",
+    color: '#bdbdbd'
   },
-  ButtonGroup:{
+  ButtonGroup: {
     flexDirection: "row",
   },
-  FollowButton:{
-    backgroundColor:FOLLOW_COLOR,
+  FollowButton: {
+    backgroundColor: FOLLOW_COLOR,
     height: 30,
     width: 120,
     borderRadius: 20,
     justifyContent: "center",
-    alignItems :"center",
+    alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -212,21 +249,21 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.30,
     shadowRadius: 4.65,
-    
+
     elevation: 8,
-    },
-  FollowText:{
-    fontSize: 13,
-    color:'white'
   },
-  SendMessageButton:{
-    marginLeft:5,
-    backgroundColor:SEND_MESSAGE_COLOR,
+  FollowText: {
+    fontSize: 13,
+    color: 'white'
+  },
+  SendMessageButton: {
+    marginLeft: 5,
+    backgroundColor: SEND_MESSAGE_COLOR,
     height: 30,
     width: 60,
     borderRadius: 20,
     justifyContent: "center",
-    alignItems :"center",
+    alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -234,54 +271,54 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.30,
     shadowRadius: 4.65,
-    
+
     elevation: 8,
-  },  
+  },
   CountGroup: {
     flex: 0.15,
-    flexDirection:'row',
+    flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop:10,
-    alignItems:"center",
+    marginTop: 10,
+    alignItems: "center",
   },
-  Amount:{
+  Amount: {
     fontSize: 20,
     fontWeight: '400',
     textAlign: "center",
   },
-  CountText:{
+  CountText: {
     fontSize: 13,
     fontWeight: '400',
     color: '#bdbdbd',
     textAlign: "center",
   },
-  ScrollImages:{
-    flex:0.35,
+  ScrollImages: {
+    flex: 0.35,
   },
   ImageGroup: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center'
   },
-  ImgLeftCol:{
-    flexDirection:"column",
+  ImgLeftCol: {
+    flexDirection: "column",
   },
-  ImgRightCol:{
-    flexDirection:"column",
+  ImgRightCol: {
+    flexDirection: "column",
   },
-  image:{
+  image: {
     marginTop: 20,
     marginBottom: 10,
-    height:180,
-    width:180,
+    height: 180,
+    width: 180,
     borderRadius: 30
   },
 
 
-  FooterBar:{
-    marginLeft:20,
-    marginRight:20,
-    flex:0.1,
+  FooterBar: {
+    marginLeft: 20,
+    marginRight: 20,
+    flex: 0.1,
     flexDirection: "row",
     justifyContent: 'space-around',
   }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Week2
-DevC
+# Profile App
+This is a profile page application using React Native & Expo.
+
+**Time spent:**: 4 hours spent in total.
+
+Completed user stories:
+
+	 * [x] Required: Has a beauty avatar.
+	 * [x] Required: Display user's full name and current job.
+	 * [x] Required: Has Follow button and Send message button. Alert 'followed' when press on follow button, alert 'message sended' when press on Send message button.
+	 * [x] Required: Display total photos, follower , follwing (hard code).
+	 * [x] Required: List out user's beautiful photos.
+	 * [x] Optional: Style follow button and send message button has shadow.
+	 * [x] Optional: Create bottom tab layout 
+	 * [x] Optional:
+
+**Hướng dẫn:** 
+![Week1GIF](https://user-images.githubusercontent.com/48592936/61394062-8b0acd80-a8ec-11e9-82ed-3166a2e9f0a3.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
